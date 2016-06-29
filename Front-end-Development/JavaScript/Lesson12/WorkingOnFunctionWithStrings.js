7880ae3b5d249cd22cbf084861b0b7888957e6a8
Ibad Siddiqui <ibadsiddiqui01@outlook.com>
2016-06-29 10:14:23 +0500

    Strings attached to functioons

---
+++ Front-end-Development/JavaScript/Lesson12/WorkingOnFunctionWithStrings.js
@@ -0,0 +1,12 @@
+
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+  var result = "";
+  // Your code below this line
+  
+  result = "The " + myAdjective + " " + myNoun + " got " + myVerb + " " + myAdverb; 
+  // Your code above this line
+  return result;
+}
+
+// Change the words here to test your function
+wordBlanks("cat", "lttle", "hit", "slowly");
