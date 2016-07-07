e7cf074d41c09a436285b6c8a7ebedd7d9b21369
Ibad Siddiqui <ibadsiddiqui01@outlook.com>
2016-07-07 11:02:22 +0500

    working With JSON and Arrays

---
+++ Front-end-Development/JavaScript/Lesson19/ManipulatingObjects.js
@@ -0,0 +1,21 @@
+
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [ 
+      "CS", 
+      "8T", 
+      "LP" ],
+    "gold": true
+  },
+  {
+    "artist": "Eminem",
+    "title" : "Love the way you lie",
+    "release_year" : 2010,
+    "formats" : [ "LP", "CS"]
+  }
+  // Add record here
+];
+
