3174e728c19208713ba9ed1c20427bba81a0ceea
Ibad Siddiqui <ibadsiddiqui01@outlook.com>
2016-07-10 15:29:34 +0500

    declaring objects with parameters

---
+++ Front-end-Development/Object Oriented Javascript/Lesson4/ObjectWithParameters.js
@@ -0,0 +1,9 @@
+var Car = function(wheel, seat, engines) {
+    //Change this constructor
+    this.wheels = wheel;
+    this.seats = seat;
+    this.engines = engines;
+};
+
+//Try it out here
+var myCar = new Car(4, 5, 1);
