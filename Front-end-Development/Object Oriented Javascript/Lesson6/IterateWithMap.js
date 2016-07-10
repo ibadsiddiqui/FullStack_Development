9adf15cb6434770a5127c7f0d54066a5a90c6780
Ibad Siddiqui <ibadsiddiqui01@outlook.com>
2016-07-10 23:12:36 +0500

    iterating the array with .map method

---
+++ Front-end-Development/Object Oriented Javascript/Lesson6/IterateWithMap.js
@@ -0,0 +1,9 @@
+var oldArray = [1, 2, 3];
+// only change the code below
+
+var newArray = oldArray.map(function(value) {
+    return value * 4;
+});
+
+console.log(oldArray); // [1,2,3]
+console.log(newArray); //[4.8,12]
