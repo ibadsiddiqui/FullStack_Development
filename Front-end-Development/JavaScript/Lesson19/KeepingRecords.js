dc4a5fe78003c8ba7d386075e2f8cd5c967e0ce9
Ibad Siddiqui <ibadsiddiqui01@outlook.com>
2016-07-10 13:05:03 +0500

    KeepingRecords.js was updated

--- Front-end-Development/JavaScript/Lesson19/KeepingRecords.js
+++ Front-end-Development/JavaScript/Lesson19/KeepingRecords.js
@@ -1,28 +1,27 @@
-
 // Setup
 var collection = {
-    "2548": {
-      "album": "Slippery When Wet",
-      "artist": "Bon Jovi",
-      "tracks": [ 
-        "Let It Rock", 
-        "You Give Love a Bad Name" 
-      ]
+    2548: {
+        album: "Slippery When Wet",
+        artist: "Bon Jovi",
+        tracks: [
+            "Let It Rock",
+            "You Give Love a Bad Name"
+        ]
     },
-    "2468": {
-      "album": "1999",
-      "artist": "Prince",
-      "tracks": [ 
-        "1999", 
-        "Little Red Corvette" 
-      ]
+    2468: {
+        album: "1999",
+        artist: "Prince",
+        tracks: [
+            "1999",
+            "Little Red Corvette"
+        ]
     },
-    "1245": {
-      "artist": "Robert Palmer",
-      "tracks": [ ]
+    1245: {
+        artist: "Robert Palmer",
+        tracks: []
     },
-    "5439": {
-      "album": "ABBA Gold"
+    5439: {
+        album: "ABBA Gold"
     }
 };
 // Keep a copy of the collection for tests
@@ -30,15 +29,18 @@ var collectionCopy = JSON.parse(JSON.stringify(collection));
 
 // Only change code below this line
 function updateRecords(id, prop, value) {
-  if(id == collection[id])
-    delete colletion[id][prop];
-  else if(prop != 'tracks')
-    collection[id][prop] = value;
-  else
-    collection[id][prop].push(value);
-  return collection;
+    if (prop === "tracks" && value !== "")
+        if (collection[id][prop])
+            collection[id][prop].push(value);
+        else
+            collection[id][prop] = [value];
+    else if (value !== "")
+        collection[id][prop] = value;
+    else
+        delete collection[id][prop];
+
+
+    return collection;
 }
-
 // Alter values below to test your code
-updateRecords(5439, "artist", "ABBA");
-
+update(5439, "artist", "ABBA");
