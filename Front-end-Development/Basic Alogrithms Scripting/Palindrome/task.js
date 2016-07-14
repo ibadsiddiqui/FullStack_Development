301288a53550768034100a89fd68aad7a0b7f513
Ibad Siddiqui <ibadsiddiqui01@outlook.com>
2016-07-14 10:59:32 +0500

    Palindrome task completed

---
+++ Front-end-Development/Basic Alogrithms Scripting/Palindrome/task.js
@@ -0,0 +1,5 @@
+function palindrome(str) {
+  var normalizedStr = str.replace(/[\W_]/g, '').toLowerCase();
+  var reverseStr = normalizedStr.split('').reverse().join('');
+  return normalizedStr === reverseStr;
+}
