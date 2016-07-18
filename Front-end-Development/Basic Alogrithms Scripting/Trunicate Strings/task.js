3c7845851359eab5cccd0fac8364136fa3112031
Ibad Siddiqui <ibadsiddiqui01@outlook.com>
2016-07-18 22:22:12 +0500

    trunicate the string with given length

---
+++ Front-end-Development/Basic Alogrithms Scripting/Trunicate Strings/task.js
@@ -0,0 +1,10 @@
+function truncString(str, num) {
+    if (str.length > num && num >= 3) {
+        return str.slice(0, num - 3) + "...";
+    }
+    if (str.length > num && num <= 3) {
+        return str.slice(0, num) + "...";
+    }
+    return str;
+}
+truncString("The string is real", 5);
